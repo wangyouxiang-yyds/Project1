614811c134868d9b6989e06514a345aadb29fb22
wang <tree8454@gmail.com>
2025-10-04 16:00:26 +0800

    fixed test data prepeocssing

--- data_cleaning_preprocess.py
+++ data_cleaning_preprocess.py
@@ -1,31 +1,39 @@
 import pandas as pd
 import numpy as np
 
-def data_preprocessing(data_file):
-    data = pd.read_csv(data_file)
+def data_preprocessing(data_file, test_file):
+    data_train = pd.read_csv(data_file)
+    data_test = pd.read_csv(test_file)
 
     # 刪除不必要的欄位
-    data = data.drop(columns=["ID", "fullAddress", "postcode", "country", "sale_month"])
+    drop_columns = ["ID", "fullAddress", "postcode", "country", "sale_month"]
+    data_train = data_train.drop(columns=drop_columns)
+    data_test = data_test.drop(columns=drop_columns)
 
 
     # 資料做中位數填補
     for col in ["bathrooms", "bedrooms", "livingRooms", "floorAreaSqM"]:
-        data[col] = data[col].fillna(data[col].median())
+        data_train[col] = data_train[col].fillna(data_train[col].median())
+        data_test[col] = data_test[col].fillna(data_test[col].median())
 
     # 填補類別補上缺失
     for col in ["tenure", "propertyType", "currentEnergyRating"]:
-        data[col] = data[col].fillna("Unknown")
+        data_train[col] = data_train[col].fillna("Unknown")
+        data_test[col] = data_test[col].fillna("Unknown")
 
-    data["sale_year"] = 2025 - data["sale_year"]
+    data_train["sale_year"] = 2025 - data_train["sale_year"]
+    data_test["sale_year"] = 2025 - data_test["sale_year"]
 
     # price 做 log 結束後記得要補回來
-    data["price"] = np.log1p(data["price"])
-    print(f"data Nan in total :{data.isnull().sum()}")
-    return data
+    data_train["price"] = np.log1p(data_train["price"])
+    print(f"train data Nan in total :{data_train.isnull().sum()}")
+    print(f"test data Nan in total :{data_test.isnull().sum()}")
+    return data_train, data_test
 
 
 if __name__ == "__main__":
 
     data_file = "train.csv"
-    data_preprocessing(data_file)
+    test_file = "test.csv"
+    data_preprocessing(data_file, test_file)
     
