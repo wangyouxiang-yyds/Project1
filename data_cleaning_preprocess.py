4c8782ab828bcbc9012c44245977bc2c2799ac4b
wang <tree8454@gmail.com>
2025-10-04 15:35:42 +0800

    fixed data clean preprocess

--- data_cleaning_preprocess.py
+++ data_cleaning_preprocess.py
@@ -4,23 +4,28 @@ import numpy as np
 def data_preprocessing(data_file):
     data = pd.read_csv(data_file)
 
+    # 刪除不必要的欄位
+    data = data.drop(columns=["ID", "fullAddress", "postcode", "country", "sale_month"])
+
+
     # 資料做中位數填補
     for col in ["bathrooms", "bedrooms", "livingRooms", "floorAreaSqM"]:
-        data[col].fillna(data[col].median(), inplace=True) 
+        data[col] = data[col].fillna(data[col].median())
 
     # 填補類別補上缺失
     for col in ["tenure", "propertyType", "currentEnergyRating"]:
-        data[col].fillna("Unknown", inplace=True)
+        data[col] = data[col].fillna("Unknown")
 
-    # 刪除不必要的欄位
-    data = data.drop(columns=["ID", "fullAddress", "postcode"])
+    data["sale_year"] = 2025 - data["sale_year"]
 
-    # data["price"] = np.log1p(data["price"])
-
+    # price 做 log 結束後記得要補回來
+    data["price"] = np.log1p(data["price"])
+    print(f"data Nan in total :{data.isnull().sum()}")
     return data
 
 
 if __name__ == "__main__":
 
     data_file = "train.csv"
-    print(data_preprocessing(data_file))
+    data_preprocessing(data_file)
+    
